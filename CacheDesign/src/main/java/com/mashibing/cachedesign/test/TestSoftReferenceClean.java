1a0ea1280aafede99a3c9030c498db47c33b21a6
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-03-09 14:08:33 +0800

    缓存设计：软引用清理机制：当空间不足时，就会去清理

--- CacheDesign/src/main/java/com/mashibing/cachedesign/test/TestSoftReferenceClean.java
+++ CacheDesign/src/main/java/com/mashibing/cachedesign/test/TestSoftReferenceClean.java
@@ -1,5 +1,62 @@
-package com.mashibing.cachedesign.test;/**
-@author xcy    
-@date 2023/3/7 - 7:50     
-*/    public class TestSoftReferenceClean {
+package com.mashibing.cachedesign.test;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 测试软引用清理
+ *
+ * @author xcy
+ * @date 2023/3/7 - 7:50
+ */
+public class TestSoftReferenceClean {
+	public static void main(String[] args) {
+		soft();
+	}
+
+	public static void soft() {
+		HashMap<Integer, SoftRefedStudent> map = new HashMap<>();
+		ReferenceQueue<Student> queue = new ReferenceQueue<>();
+
+		int i = 0;
+		while (i < 10000000) {
+			Student student = new Student();
+			map.put(i, new SoftRefedStudent(i, student, queue));
+
+			SoftRefedStudent pollRef = (SoftRefedStudent) queue.poll();
+			if (pollRef != null) {
+				System.out.println("回收：" + pollRef.key);
+				map.remove(pollRef.key);
+
+				System.out.println(i + "新一轮============================");
+				Iterator<Map.Entry<Integer, SoftRefedStudent>> iterator = map.entrySet().iterator();
+				while (iterator.hasNext()) {
+					Map.Entry<Integer, SoftRefedStudent> next = iterator.next();
+					if ((int) next.getValue().key == pollRef.key) {
+						System.out.println("出错了");
+					}
+				}
+				System.out.println(i + "新一轮========================");
+			}
+			i++;
+		}
+	}
+
+}
+
+class Student {
+	
+}
+
+class SoftRefedStudent extends SoftReference<Student> {
+
+	public Integer key;
+
+	public SoftRefedStudent(Integer key, Student referent, ReferenceQueue<? super Student> q) {
+		super(referent, q);
+		this.key = key;
+	}
 }
